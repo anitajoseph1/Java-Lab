13ad3bd36977873bc3fd25173e4884ccbf686956
anitajoseph1 <145443646+anitajoseph1@users.noreply.github.com>
2023-09-19 15:05:37 +0530

    My First Java Project

---
+++ HelloJava.java
@@ -0,0 +1,13 @@
+/****************************************************************
+ * File : HelloJava.java
+ * Description : My first Java Program
+ * Author : Anita Mary Joseph
+ * Date : 19/09/23
+ *****************************************************************/
+public class HelloJava {
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+		System.out.print("You are welcome");
+	}
+
+}
